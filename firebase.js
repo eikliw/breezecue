e8f2fa78b71a51854d8b1607cde3b9bd426b847b
eikliw <eikliw@gmail.com>
2025-05-11 15:54:55 -0400

    feat: integrate Firebase configuration and services into the project, utilizing environment variables for sensitive data

--- firebase.js
+++ firebase.js
@@ -1,10 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyClMM9xeQu5P0609ThT6-LYC0qxJKApyq0",
-    authDomain: "breezecue.firebaseapp.com",
-    projectId: "breezecue",
-    storageBucket: "breezecue.firebasestorage.app",
-    messagingSenderId: "985352175938",
-    appId: "1:985352175938:web:d0b7a9b2c740ba9df888db",
-    measurementId: "G-ZJ4PQY8FK0"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Export a an object with the services
+export { db, auth };
